6d18273f970de7ea9b172e3ad5e043f125ea8d19
Jesse Moses <jessemoses71@gmail.com>
2022-09-11 22:01:02 +0100

    Made prodile details more scalable and increased profile pic size.

--- src/App.js
+++ src/App.js
@@ -24,14 +24,13 @@ function App() {
   };
   const capture = () => {
     document.getElementById("canvas").classList.add("canvas--export");
-    document.getElementById("canvas-profile-details").style.marginTop = "-15%";
-    document.getElementById("verify-icon").classList.add("mt-[24px]");
-    document.getElementById("profile-username").classList.add("mt-[-4px]");
 
     html2canvas(document.getElementById("canvas"), {
       useCORS: true,
       windowWidth: 1367,
       windowHeight: 656,
+      width: 720,
+      height: 440,
     }).then((canvas) => {
       let a = document.createElement("a");
       a.href = canvas.toDataURL("image/png");
@@ -39,9 +38,6 @@ function App() {
       a.click();
 
       document.getElementById("canvas").classList.remove("canvas--export");
-      document.getElementById("canvas-profile-details").style.marginTop = "0";
-      document.getElementById("verify-icon").classList.remove("mt-[24px]");
-      document.getElementById("profile-username").classList.remove("mt-[-4px]");
     });
   };
   return (
@@ -75,8 +71,8 @@ function App() {
                 />
               </svg>
             </div>
-            <div className="header__details flex flex-col items-end leading-[100%] text-sm">
-              <div className="header__details__version">v1.1.0</div>
+            <div className="header__details flex flex-col items-end leading-[100%] text-sm font-semibold">
+              <div className="header__details__version">v1.0.1</div>
               <div className="header__details__developer flex items-center">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -260,32 +256,29 @@ function App() {
           }
         >
           <div
-            className="canvas flex items-center justify-center"
+            className="canvas relative flex items-center justify-center"
             id="canvas"
             style={{ background: activeStyle[0], color: activeStyle[1] }}
           >
+            {/* Overlay */}
             <div
               className={
                 activeButton === "picture" ? "overlay" : "overlay hidden"
               }
             ></div>
+            {/* Canvas content */}
             <div className="canvas-text-wrapper w-full">
-              <div className="canvas-header flex items-center justify-start w-fit ">
+              <div className="canvas-header flex items-center ">
                 <div className="canvas-profile-pic rounded-full"></div>
-                <div
-                  className="canvas-profile-details ml-2"
-                  id="canvas-profile-details"
-                >
-                  <div className="flex items-center justify-center">
-                    <span className="text-2xl font-normal leading-none">
-                      Big Yarns
-                    </span>
-                    <div className="h-fit w-fit mt-1 ml-1" id="verify-icon">
+                <div className="canvas-profile-details flex flex-col h-full ml-3 leading-none">
+                  <div className="text-3xl flex items-center">
+                    <span className="">Big Yarns</span>
+                    <div className="verified-icon ml-1 flex items-center h-fit w-fit">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         version="1.1"
                         viewBox="0,0,24,24"
-                        className="h-[20px] w-[20px]"
+                        className="h-[24px]"
                       >
                         <path
                           d="M22.5 12.5c0-1.58-.875-2.95-2.148-3.6.154-.435.238-.905.238-1.4 0-2.21-1.71-3.998-3.818-3.998-.47 0-.92.084-1.336.25C14.818 2.415 13.51 1.5 12 1.5s-2.816.917-3.437 2.25c-.415-.165-.866-.25-1.336-.25-2.11 0-3.818 1.79-3.818 4 0 .494.083.964.237 1.4-1.272.65-2.147 2.018-2.147 3.6 0 1.495.782 2.798 1.942 3.486-.02.17-.032.34-.032.514 0 2.21 1.708 4 3.818 4 .47 0 .92-.086 1.335-.25.62 1.334 1.926 2.25 3.437 2.25 1.512 0 2.818-.916 3.437-2.25.415.163.865.248 1.336.248 2.11 0 3.818-1.79 3.818-4 0-.174-.012-.344-.033-.513 1.158-.687 1.943-1.99 1.943-3.484zm-6.616-3.334l-4.334 6.5c-.145.217-.382.334-.625.334-.143 0-.288-.04-.416-.126l-.115-.094-2.415-2.415c-.293-.293-.293-.768 0-1.06s.768-.294 1.06 0l1.77 1.767 3.825-5.74c.23-.345.696-.436 1.04-.207.346.23.44.696.21 1.04z"
@@ -294,15 +287,10 @@ function App() {
                       </svg>
                     </div>
                   </div>
-                  <p
-                    className="text-md mt-1 leading-none font-light"
-                    id="profile-username"
-                  >
-                    @bigyarns_
-                  </p>
+                  <div className="text-md ">@bigyanrs_</div>
                 </div>
               </div>
-              <p className="canvas-text w-full text-3xl mt-5 font-light">
+              <p className="canvas-text w-full text-3xl  font-light">
                 {quoteText ||
                   "Success is nothing more than a few simple disciplines, practiced every day."}
               </p>
