48c1573f318cd956124d4be737a65ce9daa7c169
Jesse Moses <jessemoses71@gmail.com>
2022-09-11 22:45:27 +0100

    Compatibility tweaks

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ function App() {
                   stroke-width="2.5"
                   stroke-linecap="round"
                   stroke-linejoin="round"
-                  class="feather feather-twitter"
+                  class="feather feather-twitter mr-0.5"
                 >
                   <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
                 </svg>
@@ -308,7 +308,7 @@ function App() {
           <div className="small-screen__text text-center mb-8">
             {restrictedView
               ? "Please exit the browser."
-              : "This app may misbehave on your device, will you proceed anyways?"}
+              : "Mobile previews aren't always acurate, will you proceed?"}
           </div>
           <div className="small-screen__button-wrapper justify-center flex w-full">
             {!restrictedView && (
